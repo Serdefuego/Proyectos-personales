cfec08983204f93bc5b0a2de0adf2af2a1a8ca83
Marcos <mnlmutan@gmail.com>
2025-12-11 10:02:52 -0300

    agregando boton para eliminar tarea del la lista

--- CRUD_PENDIENTES/script.js
+++ CRUD_PENDIENTES/script.js
@@ -1,4 +1,5 @@
-
+   /********************************************************************/
+    /********************************************************************/
 //capturo el evento click del boton para agregar tarea
 document.getElementById("agregar").addEventListener("click",()=>{
     //capturo en una variable el elemento <ul> ="lista"
@@ -8,19 +9,32 @@ document.getElementById("agregar").addEventListener("click",()=>{
     //creo un elemento <li> para meterlo dentro de la <ul>
     let tarea=document.createElement("li")
     //modifico el textConntent del elemento <li> que voy a meter dentro del <ul> 
-    tarea.textContent=descripcion
-    //agrego mediante append en la lista <ul> la tarea 
+    tarea.append(descripcion)
 
-    if(tarea.textContent==""){
+    /*********************************************************************/
+    //creo el boton que ira dentro de cada <li> "tarea" de la "lista"
+    let remover=document.createElement("button")
+    //le agrego el texto "eliminar" al boton 
+    remover.textContent="eliminar"
+    //agrega con un append el boton "remover"
+    tarea.append(remover)
+    //capturo el click que se le hace al boton de remover
+    remover.addEventListener("click",()=>{
+        tarea.remove()
+    })
+    /********************************************************************/
+
+    //Evitar agregar tareas vacías 
+    if(descripcion==""){
         alert("todavia no has agragado una tarea")
         alert("escribe una tarea en el campo de texto y luego pulsa agregar")
     }else{
+        //agrego mediante append en la lista <ul> la tarea 
         lista.append(tarea)
-        descripcion.value=""
-    }
-
+        //luego de agregar la lista vacio el campo de escritura
+        document.getElementById("descripccion").value=""
+      }
     
 })
 
-//Evitar agregar tareas vacías 
-// //Si el usuario aprieta “agregar” con el input vacío, no debe crear un li vacío.
+
